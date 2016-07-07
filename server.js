ba84ad9800c3e131fa2f49d9ba975be32a594987
stephanievidal <stephanievidal259@gmail.com>
2016-07-07 08:51:21 -0500

    Lab03 IP-PORT Server con ip y puerto tomados del entorno

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-//http
+// http
 var http = require("http");
-//crear un servidor basico
+// Obteniendo información del entorno
+// De ejecución con respecto al IP
+// y al puerto que debemos usar en
+// nuestro server.
+var PORT = process.env.PORT;
+var IP = process.env.IP || `127.0.0.1`;
+if(IP==`127.0.0.1`){
+    console.log("> ---- EJECUTANDO EN MODO LOCAL ----");
+}
+
+// Crear un servidor basico
 var server = http.createServer(function(req, res){
     // Armar la respuesta http
     // Armar un encabezado http
-    res.writeHead(200, {
-        "Content-Type" : "text/plain", 
+    res.writeHead(200,{
+        "Content-Type" : "text/plain",
         "Server" : "ITGAM@4.2.4"
     });
     // Enviamos la respuesta
-    res.write("Hola Mundo");
-    //Cerrar la conexion
+    res.write("Stephanie Vidal Server");
+    // Cerrar la conexion
     res.end();
 });
-//Poner a trabajar el server
-server.listen(3000, `127.0.0.1`, function(){
-    console.log("> Server listening @http://localhost:3000 ...");
+// Poner a trabjar al server
+server.listen(PORT,IP,function(){
+    console.log(`> Server listening @http://${IP}:${PORT} ...`);
 });
