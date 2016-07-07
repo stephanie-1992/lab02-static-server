e8b366af7dafae94acee3ddd20035366c7ec896b
stephanievidal <stephanievidal259@gmail.com>
2016-07-07 10:04:10 -0500

    lab05 Server con HTML fijo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // http
 var http = require("http");
+var fs = require('fs');
 // Obteniendo información del entorno
 // De ejecución con respecto al IP
 // y al puerto que debemos usar en
@@ -15,13 +16,19 @@ var server = http.createServer(function(req, res){
     // Armar la respuesta http
     // Armar un encabezado http
     res.writeHead(200,{
-        "Content-Type" : "text/plain",
+        "Content-Type" : "text/html",
         "Server" : "ITGAM@4.2.4"
     });
-    // Enviamos la respuesta
-    res.write("Stephanie Vidal Server");
-    // Cerrar la conexion
-    res.end();
+
+    // lectura del archivo a servir
+    fs.readFile(`./static/index.html/`,
+    `utf8`,function(err, content){
+        if(err){
+            res.write("<h1>ERROR DE LECTURA</h1>");
+        }else{
+            res.end(content);
+        }
+    });    
 });
 // Poner a trabjar al server
 server.listen(PORT,IP,function(){
